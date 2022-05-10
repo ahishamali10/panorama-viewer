bd0e4539110beecc0eb8d43fefb2b249c73eddf7
Hisham Ali Alshami <ahishamali10@gmail.com>
2022-05-10 22:40:17 +0300

    npm prod

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -36,7 +36,7 @@ const toggle = ref('image')
 
         <div class="w-10/12 mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h1 class="text-white dark:text-gray-900 text-6xl">Panorama View Example</h1>
+                <h1 class="dark:text-white text-gray-900 text-6xl">Panorama View Example</h1>
             </div>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
