5a325f88766bfea20fb69b8d4692c0afd8e8de03
Hisham Ali Alshami <ahishamali10@gmail.com>
2022-05-12 20:12:47 +0300

    fixing bugs

--- resources/js/Components/PanoImage.vue
+++ resources/js/Components/PanoImage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full h-80">
-        <div id="pano-i" class="w-full h-full"></div>
+        <div :id="`pano-i-${number}`" class="w-full h-full"></div>
     </div>
 </template>
 
@@ -16,13 +16,14 @@ export default {
             type: String,
             default: 'image'
         },
-        source: ''
+        source: '',
+        number: 0,
     },
 
     setup(props) {
         onMounted(() => {
             const panorama = new PANOLENS.ImagePanorama(props.source);
-            let container = document.querySelector('#pano-i');
+            let container = document.querySelector(`#pano-i-${props.number}`);
             container.innerHTML = ''
             const viewer = new PANOLENS.Viewer({
                 container: container,
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -35,7 +35,7 @@ const toggle = ref('image')
 <!--            </template>-->
 <!--        </div>-->
 
-        <div class="w-10/12 mx-auto sm:px-6 lg:px-8">
+        <div class="w-10/12 mx-auto py-20 sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <h1 class="dark:text-white text-gray-900 text-6xl">Panorama View Example</h1>
             </div>
@@ -59,9 +59,7 @@ const toggle = ref('image')
 
                         <div>
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm" :class="[toggle === 'video' ? 'bg-white' : '']">
-                                <pano-image v-if="toggle === 'image'" source="/360/images/test1.jpg" type="image"/>
-                                <img v-else src="/images/panorama/img.png"
-                                     class="h-80 w-full"/>
+                                <pano-image number="1" source="/360/images/test1.jpg" type="image"/>
                             </div>
                         </div>
                     </div>
@@ -83,9 +81,51 @@ const toggle = ref('image')
 
                         <div>
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm" :class="[toggle === 'image' ? 'bg-white' : '']">
-                                <pano-video v-if="toggle === 'video'" source="/360/images/test2.jpg" type="image"/>
-                                <img v-else src="/images/panorama/img.png"
-                                     class="h-80 w-full"/>
+                                <pano-image number="2" source="/360/images/test2.jpg" type="image"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                 stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
+                                <path
+                                    d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
+                                <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            </svg>
+                            <div class="ml-4 text-lg leading-7 font-semibold">
+                                <span @click="toggle = 'image'" class="cursor-pointer underline text-gray-900 dark:text-white">
+                                    Image 3 Panorama
+                                </span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm" :class="[toggle === 'video' ? 'bg-white' : '']">
+                                <pano-image source="/360/images/test3.jpg" number="3" type="image"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                 stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
+                                <path
+                                    d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
+                                <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            </svg>
+                            <div class="ml-4 text-lg leading-7 font-semibold">
+                                <span @click="toggle = 'video'" class="cursor-pointer underline text-gray-900 dark:text-white">
+                                    Image 4 Panorama
+                                </span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm" :class="[toggle === 'image' ? 'bg-white' : '']">
+                                <pano-image number="4" source="/360/images/test4.jpg" type="image"/>
                             </div>
                         </div>
                     </div>
