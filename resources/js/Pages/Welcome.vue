1ba26b634d74f1e4b5675fc2122d13b58c6fefd5
Hisham Ali Alshami <ahishamali10@gmail.com>
2022-05-12 20:40:54 +0300

    fixing bugs

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -86,7 +86,7 @@ const toggle = ref('image')
                         </div>
                     </div>
 
-                    <div class="p-6">
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                         <div class="flex items-center">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                  stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
@@ -108,7 +108,7 @@ const toggle = ref('image')
                         </div>
                     </div>
 
-                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                         <div class="flex items-center">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                  stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
